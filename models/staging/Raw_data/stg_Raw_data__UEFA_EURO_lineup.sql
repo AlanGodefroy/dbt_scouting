0f69afa67aa32c3ea183ee4705e20efcd79798a4
Alan Godefroy <alan.godefroy@gmail.com>
2026-03-12 16:19:16 +0000

    ss

--- models/staging/Raw_data/stg_Raw_data__UEFA_EURO_lineup.sql
+++ models/staging/Raw_data/stg_Raw_data__UEFA_EURO_lineup.sql
@@ -9,11 +9,8 @@ source as (
 renamed as (
 
     select
-<<<<<<< HEAD
         CAST(player_id AS INT64) AS player_id,
-=======
         CAST(TRUNC(player_id) AS INT64) AS player_id,
->>>>>>> 7efaf2181c5683624416ab7163cebe8660870c0d
         player_name,
         player_nickname,
         jersey_number,
